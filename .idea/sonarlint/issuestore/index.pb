d1c57690d65730570de6499fc6470b1c76832b00
odarochka <odarochka1802@gmail.com>
2023-09-20 15:38:15 +0300

    all

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,69 @@
+
+^
+..idea/inspectionProfiles/profiles_settings.xml,1\e\1e9075f5bf079c01ef2c910709e91a497d262080
+H
+Seminar1/Home1/manage.py,3\b\3bfc27cdaf85992bace737adad66985cb812d550
+L
+Seminar1/Home1/shop/admin.py,4\1\41402298399a14d58c44707e4d8d798458bfbf49
+K
+Seminar1/Home1/shop/apps.py,4\b\4b1f34164290a8d7d72cea69e476239798bb6574
+H
+Seminar1/First/manage.py,6\1\6113b6d761d16e7fe22114ef61924f3005fb0bcc
+L
+Seminar1/First/First/asgi.py,c\3\c3d923a2a055eed0a5fd05b0ac80c14191d378b6
+M
+Seminar1/Home1/shop/models.py,2\5\2594f6be9c665023dfe53490e824543845b8dc1b
+P
+ Seminar1/First/First/settings.py,d\3\d39440f2bd486f30e9f0f749aae7620aa57545a9
+L
+Seminar1/Home1/shop/tests.py,7\8\784466903a4fcbfbc819e829bd694ca845c1e0a7
+L
+Seminar1/First/First/wsgi.py,e\e\eeafcd6677cb9d70f8b1bbc480ca40f1da0d8f3c
+O
+Seminar1/Home1/shop/__init__.py,9\4\94767f8032cecee8edb749442eaa9b19d29f02f5
+P
+ Seminar1/First/First/__init__.py,b\9\b9ee773458eddaeb7146b86ff675ed0798d89047
+Z
+*Seminar1/Home1/shop/migrations/__init__.py,6\c\6c8975f45ebb9613ae726168a7887eefbe4caa6e
+L
+Seminar1/First/Sem1/admin.py,4\0\409b7dc7de15904f03a7f55f1981454f5831fc26
+I
+Seminar1/Home1/db.sqlite3,a\2\a276483d94e182f571a98b2c907e05b6138e0f67
+K
+Seminar1/First/Sem1/apps.py,1\7\17637684ea883e015192b2444dd55e6b82c4c490
+M
+Seminar1/First/Sem1/models.py,4\b\4b7aa81414395108fa67ca1e98df1916c12686f6
+P
+ Seminar1/Home1/Home1/__init__.py,5\6\567776fe32a9b1a42e4b829b398acbbff8818d7e
+L
+Seminar1/First/Sem1/tests.py,2\8\28fee08b3267686737a3623c0f2837327207128f
+L
+Seminar1/Home1/Home1/asgi.py,d\6\d69b54c107f85df6091c0329e1c6b17bcef97633
+O
+Seminar1/First/Sem1/__init__.py,6\3\6318cc984b08e658703859ed2de86a5a389dd095
+Z
+*Seminar1/First/Sem1/migrations/__init__.py,1\9\19818808c0898c192fe1dacef6a40e342960fc45
+I
+Seminar1/First/db.sqlite3,9\7\97db5777a2b8a632554c5121cef9d08d52624d5d
+P
+ Seminar1/Home1/MYRandom/admin.py,2\7\2734c7df3ff661d079ae3bb6aa003674af167a49
+O
+Seminar1/Home1/MYRandom/apps.py,3\3\33d6c7e1e354b5ec38caaf3ba18d16c48d3568b0
+K
+Seminar1/First/Sem1/urls.py,9\f\9f0a62fbe9f75a21fa42729bc70fe904d24974be
+Q
+!Seminar1/Home1/MYRandom/models.py,4\f\4fbbe1dd8567a60046b633bf6cedae8e27054bc9
+P
+ Seminar1/Home1/MYRandom/tests.py,9\b\9b83264ca7fcd49fa1e7712f67921a484d4ec71d
+M
+Seminar1/First/log/django.log,2\1\21b514023546664b1a113f1b660c5933fc87553f
+S
+#Seminar1/Home1/MYRandom/__init__.py,2\5\25d7d1a72e40e16a399dfdce1aa0d5100bb9f845
+^
+.Seminar1/Home1/MYRandom/migrations/__init__.py,a\2\a2b292aad8a49b9af3de43785eebb7f84edc4d05
+H
+Seminar1/Home1/debug.log,3\0\3036789235932c298a2a0fb078837591b86e0b4f
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+M
+Seminar1/Home1/log/django.log,4\1\41a6c063e3f6b08324dd9b044830d6a900d89cdb
--- Seminar1/Home1/shop/views.py
+++ Seminar1/Home1/shop/views.py
@@ -5,6 +5,85 @@ from django.shortcuts import render
 
 # Create your views here.
 logger = logging.getLogger(__name__)
+
+
 def index(request):
     logger.info("OK")
-    return HttpResponse("Hello World")
+    info = """
+    <!DOCTYPE html>
+<html>
+<head>
+    <title>Название вашего интернет-магазина</title>
+    <link rel="stylesheet" href="style.css"> <!-- Подключение внешнего CSS-файла для стилизации страницы -->
+</head>
+<body style="font-family: Arial, sans-serif;line-height: 1.5;background-color: #f5f5f5;  margin: 0;padding: 0;box-sizing: border-box">
+    <header style="background-color: #333;padding: 20px;color: #fff;">
+        <h1 style="margin-bottom: 10px;">Добро пожаловать в наш интернет-магазин!</h1>
+        <nav>
+            <ul style="list-style-type: none;">
+                <li style="display: inline-block;margin-right: 15px;"><a style="text-decoration: none;color: #fff;" href="#">Главная</a></li>
+                <li style="display: inline-block;margin-right: 15px;><a  style="text-decoration: none;color: #fff;" href="#">Каталог</a></li>
+                <li style="display: inline-block;margin-right: 15px;><a  style="text-decoration: none;color: #fff;" href="#">О нас</a></li>
+                <li style="display: inline-block;margin-right: 15px;><a  style="text-decoration: none;color: #fff;" href="#">Контакты</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main style="padding: 20px;">
+        <section class="banner" style="margin-bottom: 30px; background-color: #f9f9f9;padding: 30px;text-align: center;">
+            <h2 style="margin-bottom: 10px;">Акционные предложения!</h2>
+            <p style="margin-bottom: 20px;">У нас есть лучшие товары по низким ценам. Поторопитесь сделать заказ!</p>
+            <a href="#" class="btn" style="display: inline-block;padding: 10px 20px;background-color: #333;color: #fff;text-decoration: none;border-radius: 5px;">Купить сейчас</a>
+        </section>
+        <section class="featured-products" style="display: flex;flex-wrap: wrap;">
+            <h2 style="margin-bottom: 10px;">Популярные товары</h2>
+            <div class="product" style="width: 25%;padding: 10px;text-align: center;>
+                <img src="product1.jpg" alt="Товар 1" style="width: 100%;max-height: 200px;object-fit: cover;margin-bottom: 10px;">
+                <h3 style="margin-bottom: 5px;">Название товара</h3>
+                <p style="margin-bottom: 20px;">Цена: $99.99</p>
+                <a href="#" class="btn" style="display: inline-block;padding: 10px 20px;background-color: #333;color: #fff;text-decoration: none;border-radius: 5px;">Подробнее</a>
+            </div>
+            <!-- Добавьте другие товары -->
+        </section>
+    </main>
+    <footer style=" background-color: #333;color: #fff;padding: 20px;text-align: center;">
+        <p>&copy; 2023 Мой интернет-магазин. Все права принадлежат мне))).</p>
+        </div>
+    </footer>
+</body>
+</html>
+    """
+    return HttpResponse(info)
+
+
+def about(request):
+    logger.info("about")
+    info = """<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Тег А, атрибут target</title>
+ </head>
+ <body>
+ <div style="width: 90%; border: 1px solid #333; box-shadow: 8px 8px 5px #444; padding: 8px 12px; background-image: linear-gradient(180deg, #fff, #ddd 40%, #ccc)"> 
+ <h1>Добро пожаловать в наш интернет-магазин!</h1>
+</div>
+ <div class="container">
+  <p style="padding: 20px;">Здесь вы найдете широкий выбор качественных товаров по отличным ценам. <br>Наш интернет-магазин предлагает разнообразие 
+  продуктов для всех возрастных категорий и интересов. У нас вы можете найти модную одежду, стильные аксессуары, косметику, 
+  товары для дома, электронику и многое другое. Мы тщательно подбираем товары, чтобы обеспечить высокое качество и удовлетворить потребности наших клиентов.</p>
+<p>Независимо от того, что вас интересует - модный стиль, функциональность или красота - наш интернет-магазин предлагает 
+разнообразие опций для удовлетворения ваших потребностей. Мы постоянно обновляем наш ассортимент, чтобы предложить вам новые и новейшие товары.</p>
+<p>Мы стремимся предоставить непревзойденный уровень обслуживания наших клиентов. Наша команда всегда готова помочь вам в 
+выборе товара, ответить на ваши вопросы и обеспечить приятный опыт покупок в нашем магазине.</p>
+<p>Оформление заказа у нас легкое и безопасное. Мы гарантируем безопасность ваших платежей и конфиденциальность вашей информации.</p>
+<p>Мы ценим каждого клиента и стремимся к высокому уровню удовлетворенности. Ваше мнение очень важно для нас, поэтому мы 
+всегда открыты к обратной связи, предложениям и комментариям.</p>
+<p>Мы приглашаем вас ознакомиться с нашим ассортиментом и найти то, что идеально подойдет именно для вас. Благодарим вас за выбор нашего интернет-магазина и желаем вам приятных покупок!</p>
+ </div>
+ <footer style=" background-color: #333;color: #fff;padding: 20px;text-align: center;">
+        <p>&copy; 2023 Мой интернет-магазин. Все права принадлежат мне))).</p>
+        </div>
+    </footer>
+ </body>
+</html>"""
+    return HttpResponse(info)
