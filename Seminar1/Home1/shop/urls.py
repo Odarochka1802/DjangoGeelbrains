8aeb3a31acfa350e679c12fde50bba343ba15f1d
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2023-09-27 09:44:26 +0300

    Update urls.py

--- Seminar1/Home1/shop/urls.py
+++ Seminar1/Home1/shop/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from . import views
+from .views import ShowProduct
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('catalog/', views.catalog, name='catalog'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('post/<slug:post_slug>/', ShowProduct.as_view(), name='product'),
+
 
 ]
