f4e80349aebb94a45c0461afa108535c349f374c
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2023-10-03 13:41:40 +0300

    Add files via upload

--- Seminar1/Home1/shop/urls.py
+++ Seminar1/Home1/shop/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('post/<slug:post_slug>/', ShowProduct.as_view(), name='product'),
-
+    path('order_list/<int:client_id>/', views.order_list, name='order_list'),
+    path('create_order/', views.create_order, name='create_order'),
+    path('order_success/', views.order_success, name='order_success'),
 
 ]
--- Seminar1/Home1/shop/views.py
+++ Seminar1/Home1/shop/views.py
@@ -153,12 +153,12 @@ def delete_order(order_id):
 class ShowProduct(DetailView):
     model = Product
     template_name = 'shop/product.html'
-    slug_url_kwarg = 'post_slug'
-    context_object_name = 'post'
+    slug_url_kwarg = 'title_slug'
+    context_object_name = 'description'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title=context['post'])
+        c_def = self.get_user_context(title=context['description'])
         return dict(list(context.items()) + list(c_def.items()))
 
 
